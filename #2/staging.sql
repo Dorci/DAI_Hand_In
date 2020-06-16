ade4e1cd21f2fa3eef42f832ecbfaaf4d86cddd2
David Franko <david.franko1998@gmail.com>
2020-06-16 14:39:03 +0200

    Added assignment 2

---
+++ #2/staging.sql
@@ -0,0 +1,126 @@
+-- ***********************************************
+-- ************ CREATING STAGE TABLES ************
+-- ***********************************************
+--This statement creates staging dimension table stage_dim_customer with attributes and assigned primary key as customer_id
+CREATE TABLE staging.stage_dim_customer
+(
+    dimension_customer_id INT NOT NULL IDENTITY,
+    customer_id           INT NOT NULL,
+    title                 varchar(10),
+    first_name            varchar(50),
+    middle_name           varchar(50),
+    last_name             varchar(50),
+    PRIMARY KEY (customer_id)
+);
+--This statement creates staging dimension table stage_dim_product with attributes and assigned primary key as dimension_product_id
+CREATE TABLE staging.stage_dim_product
+(
+    dimension_product_id INT NOT NULL IDENTITY,
+    product_id           INT,
+    name                 varchar(50),
+    PRIMARY KEY (dimension_product_id)
+);
+--This statement creates staging dimension table stage_dim_date with attributes and assigned primary key as dimension_date_id
+CREATE TABLE staging.stage_dim_date
+(
+    dimension_date_id INT  NOT NULL IDENTITY,
+    month_name        varchar(10),
+    day_name          varchar(10),
+    date              DATETIME NOT NULL,
+    PRIMARY KEY (dimension_date_id)
+);
+
+-- **********************************************************
+-- ************ INSERTING DATA INTO STAGE TABLES ************
+-- **********************************************************
+
+-- _______________________ CUSTOMER _______________________
+
+--This statement inserts attribute values into staging dimension table stage_dim_customer
+INSERT INTO staging.stage_dim_customer(customer_id, title, first_name, middle_name, last_name)
+SELECT CustomerID, Title, FirstName, MiddleName, LastName
+FROM AdventureWorks2017.Sales.Customer
+         JOIN AdventureWorks2017.Person.Person ON Customer.PersonID = Person.BusinessEntityID;
+
+--This statement removes null values in the title attribute in stage_dim_customer table by replacing with 'N/A' 
+UPDATE staging.stage_dim_customer
+SET title='N/A'
+WHERE title IS NULL;
+
+--This statement removes null values in the middle_name attribute in stage_dim_customer table by replacing with 'N/A' 
+UPDATE staging.stage_dim_customer
+SET middle_name='N/A'
+WHERE middle_name IS NULL;
+
+
+
+-- _______________________ PRODUCT _______________________
+
+--This statement inserts attribute values into staging dimension table stage_dim_product
+INSERT INTO staging.stage_dim_product(product_id, name)
+SELECT ProductID, Name
+from AdventureWorks2017.Production.Product;
+
+--This statement removes null values in the name attribute in stage_dim_product table by replacing with 'N/A' 
+UPDATE staging.stage_dim_product
+SET name='N/A'
+WHERE name IS NULL;
+
+
+
+-- _______________________ DATE _______________________
+
+--This statement inserts attribute values unto staging dimension table stage_dim_date
+INSERT INTO staging.stage_dim_date(date)
+SELECT distinct OrderDate
+from AdventureWorks2017.Sales.SalesOrderHeader;
+
+--This statement extracts the date name and assigns it to the day_name attribute in stage_dim_date table when the value is null 
+UPDATE staging.stage_dim_date
+SET day_name = DATENAME(weekday, date)
+WHERE day_name IS NULL;
+
+--This statement extracts the month name and assigns it to the month_name attribute in stage_dim_date table when the value is null 
+UPDATE staging.stage_dim_date
+SET month_name = DATENAME(month, date)
+WHERE month_name IS NULL;
+
+-- **********************************************************
+-- ************ INSERTING DATA INTO FACT TABLES ************
+-- **********************************************************
+
+--This statement creates staging fact table stage_f_sales with attributes and assigned primary key as sales_id
+CREATE TABLE staging.stage_f_sales
+(
+    sales_id             INT      NOT NULL IDENTITY,
+    customer_id          INT      NULL,
+    product_id           INT      NULL,
+    date_id              INT      NULL,
+    business_customer_id INT      NULL,
+    business_product_id  INT      NULL,
+    business_order_date  DATETIME NULL,
+    quantity             INT      NULL,
+    line_total           FLOAT    NULL,
+    PRIMARY KEY (sales_id)
+);
+
+--This statement inserts attribute values into staging fact table stage_f_sales
+INSERT INTO staging.stage_f_sales(business_customer_id, business_product_id, business_order_date, quantity, line_total)
+    (SELECT C.CustomerID, P.ProductID, OrderDate, SOD.OrderQty, SOD.LineTotal
+     FROM AdventureWorks2017.Sales.SalesOrderHeader SOH
+              JOIN AdventureWorks2017.Sales.SalesOrderDetail SOD on SOH.SalesOrderID = SOD.SalesOrderID
+              JOIN AdventureWorks2017.Sales.Customer C on SOH.CustomerID = C.CustomerID
+              JOIN AdventureWorks2017.Production.Product P on SOD.ProductID = P.ProductID
+     WHERE OnlineOrderFlag = 1);
+
+--This statement extracts the customer_id from staging dimesnsion table stage_dim_customer and assigns it to the customer_id attribute in stage_f_sales table when the value is null 
+UPDATE staging.stage_f_sales
+SET customer_id = (SELECT dimension_customer_id FROM staging.stage_dim_customer AS dim_C_id WHERE dim_C_id.customer_id = business_customer_id) WHERE customer_id IS NULL;
+
+--This statement extracts the product_id from staging dimesnsion table stage_dim_product and assigns it to the product_id attribute in stage_f_sales table when the value is null 
+UPDATE staging.stage_f_sales
+SET product_id = (SELECT dimension_product_id FROM staging.stage_dim_product AS dim_P_id WHERE dim_P_id.product_id = business_product_id) WHERE product_id IS NULL;
+
+--This statement extracts the date_id from staging dimesnsion table stage_dim_date and assigns it to the date_id attribute in stage_f_sales table when the value is null 
+UPDATE staging.stage_f_sales
+SET date_id = (SELECT dimension_date_id FROM staging.stage_dim_date AS dim_D_id WHERE dim_D_id.date = business_order_date) WHERE date_id IS NULL;
---
+++ #2/star_schema_table_create.sql
@@ -0,0 +1,47 @@
+--This statement creates dimension table d_customer with attributes and primary key is assigned to dimension_customer_id
+CREATE TABLE star_schema.d_customer (
+	dimension_customer_id INT NOT NULL,
+	title varchar(10),
+	first_name varchar(50) NOT NULL,
+	middle_name varchar(50),
+	last_name varchar(50) NOT NULL,
+	PRIMARY KEY (dimension_customer_id)
+);
+--This statement creates dimension table d_product with attributes and primary key is assigned to dimension_product_id
+CREATE TABLE star_schema.d_product (
+	dimension_product_id INT NOT NULL,
+	name varchar(50) NOT NULL,
+	PRIMARY KEY (dimension_product_id)
+);
+--This statement creates dimension table d_date with attributes and primary key is assigned to dimension_date_id
+CREATE TABLE star_schema.d_date (
+	dimension_date_id INT NOT NULL,
+	month_name varchar(10) NOT NULL,
+	day_name varchar(10) NOT NULL,
+	date date NOT NULL,
+	PRIMARY KEY (dimension_date_id)
+);
+--This statement creates fact table f_sales with attributes, primary key is assigned to sales_id and foreign keys customer_id, product_id
+CREATE TABLE star_schema.f_sales (
+	sales_id INT NOT NULL,
+	customer_id INT NOT NULL,
+	product_id INT NOT NULL,
+	date_id INT NOT NULL,
+	quantity INT NOT NULL,
+	line_total real NOT NULL,
+	PRIMARY KEY (sales_id),
+	FOREIGN KEY (customer_id) REFERENCES star_schema.d_customer(dimension_customer_id),
+	FOREIGN KEY (product_id) REFERENCES star_schema.d_product(dimension_product_id),
+	FOREIGN KEY (date_id) REFERENCES star_schema.d_date(dimension_date_id)
+);
+--This statement inserts attribute vales into dimension d_product table
+INSERT INTO star_schema.d_product(dimension_product_id, name)(SELECT dimension_product_id, name FROM StagingDatabase.staging.stage_dim_product);
+--This statement inserts attribute vales into dimension d_customer table
+INSERT INTO star_schema.d_customer(dimension_customer_id, title, first_name, middle_name, last_name)(SELECT dimension_customer_id, title, first_name, middle_name, last_name FROM StagingDatabase.staging.stage_dim_customer);
+--This statement inserts attribute vales into dimension d_date table
+INSERT INTO star_schema.d_date(dimension_date_id, month_name, day_name, date)(SELECT * FROM StagingDatabase.staging.stage_dim_date);
+--This statement inserts attribute vales into fact f_sales table
+INSERT INTO star_schema.f_sales(sales_id, customer_id, product_id, date_id, quantity, line_total) (SELECT sales_id, customer_id, product_id, date_id, quantity, line_total FROM StagingDatabase.staging.stage_f_sales);
+
+-- This statement returns Top seller products
+SELECT FS.product_id, name, COUNT(*) as COUNT FROM star_schema.f_sales AS FS JOIN star_schema.d_product ON FS.product_id = d_product.dimension_product_id GROUP BY FS.product_id, name ORDER BY COUNT DESC;
